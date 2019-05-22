31966fda043d090d58b23d3ec8d529600d4c18f2
Patrick Liu <patrickl@xsense.ai>
2019-05-22 06:44:18 -0700

    Catch request error and sleep

--- downloader.py
+++ downloader.py
@@ -66,36 +66,39 @@ def get_arxiv_urls(paper_titles):
         success = False
         num_trial = 0
         sleep_in_seconds = 0
+        query_results = []
         query = '{} arxiv.org'.format(paper_title)
         print(query)
         # try num_trial times with sleep_in_seconds intervals before moving on to next search
         while not success and num_trial < 2:
             time.sleep(sleep_in_seconds)
             try:
-                query_results = googlesearch.search(query, stop=1, pause=2)
+                # convert returned iterator to list to catch the error here
+                query_results = list(googlesearch.search(query, stop=1, pause=2))
             except:
                 sleep_in_seconds = random.randint(180, 240)
-                print('Warning: sleep and retrial in {} seconds'.format(sleep_in_seconds))
+                print('Warning: sleep and retry in {} seconds'.format(sleep_in_seconds))
                 continue
-            for url in query_results:
-                if 'arxiv.org/pdf' in url:
-                    # cannot scrape the title of a web page containing a pdf file
-                    continue
-                # sometimes google gives a numeric IP as of arxiv.
-                url = 'https://arxiv.org/' + '/'.join(url.replace('https://', '').split('/')[-2:])
-                print(url)
-                try:
-                    url_title = google_scrape(url)
-                except AttributeError:
-                    url_title = ''
-                print('url title ', url_title)
-                urls.append({
-                    'url_title': url_title,
-                    'url': url,
-                    'query': paper_title,
-                })
             num_trial += 1
             success = True
+
+        for url in query_results:
+            if 'arxiv.org/pdf' in url:
+                # cannot scrape the title of a web page containing a pdf file
+                continue
+            # sometimes google gives a numeric IP as of arxiv.
+            url = 'https://arxiv.org/' + '/'.join(url.replace('https://', '').split('/')[-2:])
+            try:
+                url_title = google_scrape(url)
+            except AttributeError:
+                url_title = ''
+            print('url ', url)
+            print('url title ', url_title)
+            urls.append({
+                'url_title': url_title,
+                'url': url,
+                'query': paper_title,
+            })
     with open('./papers/query_urls.json', 'a') as f_out:
         json.dump(urls, f_out, indent=4, sort_keys=True)
     return urls
